07a2442b75c17aadadf364f492af3dbc7a950a3f
hojat <hojat72elect@gmail.com>
2024-10-21 00:49:21 -0400

    Added first example for Streamlit.

---
+++ src/streamlit/README.md
@@ -0,0 +1,4 @@
+streamlit library for making a web UI with just python.
+
+I am following [this tutorial](https://www.youtube.com/watch?v=o8p7uQCGD0U)(7:43) in order to 
+write the code in this directory.
---
+++ src/streamlit/example1.py
@@ -0,0 +1,7 @@
+import streamlit
+
+# Simple examples of how to write to web browser screen.
+
+streamlit.write("Hello, World, my name is Hojat!")
+streamlit.write(3.1415926)
+streamlit.write({"key1": "value1", "key2": "value2"})
