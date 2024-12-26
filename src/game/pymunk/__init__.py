8cec1f0660325a596c420ffa9e80d3afb3fde64d
hojat <hojat72elect@gmail.com>
2024-12-26 00:46:39 -0500

    arrow example of pymunk.

---
+++ src/game/pymunk/__init__.py
@@ -0,0 +1,7 @@
+"""
+Examples using pymunk physics engine.
+
+Note that some examples require additional dependencies. Most of them use
+pygame, but some uses pyglet or other libraries for the specific task being
+shown.
+"""
