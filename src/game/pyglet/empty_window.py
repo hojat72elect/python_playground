88607f2a6c604cc28043328e5be60f4e620ed852
hojat <hojat72elect@gmail.com>
2024-12-31 10:12:43 -0500

    Moved game related stuff to their own specific repo.

--- src/game/pyglet/empty_window.py
+++
@@ -1,5 +0,0 @@
-from pyglet import app
-from pyglet.window import Window
-
-win = Window(width=740, height=360)
-app.run()
--- src/game/pymunk/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Examples using pymunk physics engine.
-
-Note that some examples require additional dependencies. Most of them use
-pygame, but some uses pyglet or other libraries for the specific task being
-shown.
-"""
