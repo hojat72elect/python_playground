070d5b028d2fb575e1cf4cdc7d24deb4db0529a4
hojat <hojat72elect@gmail.com>
2025-03-04 17:12:56 -0500

    Added an example of string concatenation in pure Python.

--- src/official_python_part1.py
+++ src/official_python_part1.py
@@ -12,6 +12,12 @@ def perform_general_math_operations():
     print(f"And the floor division of the same numbers as above is {7 // 3}")
     print(f"In python, we can raise 2 to the power of 5 like this: {2 ** 5}")
 
+def use_a_long_string():
+    merged_string = ("Hello world, my name is Hojat;\n"
+                     "And so far, I have enjoyed living on this planet.")
+    print(merged_string)
+
 
 if __name__ == '__main__':
     perform_general_math_operations()
+    use_a_long_string()
