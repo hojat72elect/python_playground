cf917a8b28b542937eb54146a89ee3b4e12d9d34
hojat <hojat72elect@gmail.com>
2025-03-04 19:35:03 -0500

    Added some tests of strings in pure python.

--- src/official_python_part1.py
+++ src/official_python_part1.py
@@ -12,12 +12,37 @@ def perform_general_math_operations():
     print(f"And the floor division of the same numbers as above is {7 // 3}")
     print(f"In python, we can raise 2 to the power of 5 like this: {2 ** 5}")
 
+
 def use_a_long_string():
     merged_string = ("Hello world, my name is Hojat;\n"
                      "And so far, I have enjoyed living on this planet.")
     print(merged_string)
 
 
+def strings_in_python():
+    example_string_1 = "Hello world!\nmy name is Hojat."
+
+    # string indexing
+    print(example_string_1[0])
+    print(example_string_1[5])
+    print(example_string_1[-1])
+    print(example_string_1[-2])
+    print(example_string_1[-6])
+
+    # string slicing
+    print(example_string_1[0:2])
+    print(example_string_1[2:5])
+    print(example_string_1[:2])
+    print(example_string_1[4:])
+    print(example_string_1[-2:])
+
+    # the built-in function len() works on strings as well
+    print(len(example_string_1))
+
+
+def lists_in_python():
+    pass
+
+
 if __name__ == '__main__':
-    perform_general_math_operations()
-    use_a_long_string()
+    lists_in_python()
