2bdd0c3dd648fad3dbdafd9e22e7ba1877efbb01
hojat <hojat72elect@gmail.com>
2025-03-08 00:22:00 -0500

    Cleaned up the implementation of LinkedList.py

--- src/dsa/LinkedList.py
+++ src/dsa/LinkedList.py
@@ -1,51 +1,52 @@
-"""
-Node is the building block for a linked list.
-"""
-
-
 class Node:
-    def __init__(self, data, nextNode=None):
-        self.data = data
-        self.nextNode = nextNode
+    """
+    Node is the building block for a linked list.
+    there are various data structures that use Node as their building block; but for the
+    sake of simplicity, and in order to avoid coupling, I'm going to use this node, ONLY for
+    linked list.
+    """
 
-    def getData(self):
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
         return self.data
 
-    def setData(self, data):
+    def set_data(self, data):
         self.data = data
 
-    def getNextNode(self):
-        return self.nextNode
+    def get_next_node(self):
+        return self.next_node
 
-    def setNextNode(self, nextNode):
-        self.nextNode = nextNode
+    def set_next_node(self, next_node):
+        self.next_node = next_node
 
     # For debug purposes
     def __repr__(self):
         return self.data
 
 
-"""
-I don't know why but looks like this LinkedList
-can only contain string values and nothing else
-"""
-
-
 class LinkedList:
+    """
+    I don't know why but looks like this LinkedList
+    can only contain string values and nothing else
+    """
+
     def __init__(self, nodes=None):
         self.head = None
         if nodes is not None:
             node = Node(data=nodes.pop(0))
             self.head = node
             for element in nodes:
-                node.nextNode = Node(data=element)
-                node = node.nextNode
+                node.next_node = Node(data=element)
+                node = node.next_node
 
     def __repr__(self):
         node = self.head
         nodes = []
         while node is not None:
             nodes.append(node.data)
-            node = node.nextNode
+            node = node.next_node
         nodes.append("None")  # as a sign that linked list has finished.
         return " -> ".join(nodes)
