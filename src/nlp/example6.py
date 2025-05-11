3813e177504678e0a08efde3d71d046c6e0875ca
hojat <hojat72elect@gmail.com>
2025-05-10 21:43:14 -0400

    Moved all NLP-related code to a specific repo.

--- src/nlp/example6.py
+++
@@ -1,6 +0,0 @@
-saying = ['After', 'all', 'is', 'said', 'and', 'done', 'more', 'is', 'said', 'than', 'done']
-tokens = set(saying)
-print(tokens)
-tokens = sorted(tokens)
-print(tokens)
-print(tokens[-2:])
